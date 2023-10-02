b661669c355b7bc69533bd36fa87460e23d4e02a
caiuscooke <cookecaius@gmail.com>
2023-10-02 13:56:09 -0400

    added logger to homepage v2

--- musicalbingo/settings/common.py
+++ musicalbingo/settings/common.py
@@ -109,7 +109,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': os.join.path(BASE_DIR, 'general.log'),
+            'filename': os.path.join(BASE_DIR, 'general.log'),
         },
     },
     'root': {
